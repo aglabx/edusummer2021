b42c9d84e8d64144f48432be716c4e217681d038
Polina <tniapp@yandex.ru>
2021-06-12 13:45:49 +0300

    add all_star_code_challenge22.py

---
+++ students/tniapp/codewars/all_star_code_challenge22.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/5865cff66b5699883f0001aa
+# All Star Code Challenge #22
+
+def number2hour_minutes(seconds):
+    return f"{seconds//3600} hour(s) and {seconds%3600//60} minute(s)"
